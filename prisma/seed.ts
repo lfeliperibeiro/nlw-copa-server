51269cb0692fcf2e59665bf8bb6e2b9b58ee1b3b
Felipe Ribeiro <piporibeiro@gmail.com>
2022-11-02 21:05:08 -0300

    feat: create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'email@email.com',
+      avatarUrl: 'https://github.com/lfeliperib.png',
+    },
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T23:21:34.538Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    },
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-05T23:21:34.538Z',
+      firstTeamCountryCode: 'AR',
+      secondTeamCountryCode: 'BR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 4,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+}
+
+main()
